6ee60f1cb25f8ee451c77b82d24af8a35619b8b4
Lorenzo Herran (portatil) <lorenzitoherran@hotmail.com>
2023-01-31 12:17:33 +0100

    gitignore update - files upload

---
+++ src/Engine_Core.h
@@ -0,0 +1,14 @@
+#pragma once
+
+//All core libraries to be included
+
+//std
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+//custom
+#include <glm.hpp>
+#include <glad/gl.h>
