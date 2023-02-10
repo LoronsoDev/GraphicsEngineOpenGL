7ddc20b8fce61b00a1117b54c18b66e884f30454
Lorenzo HS <lorenzohs98@gmail.com>
2023-02-10 13:53:34 +0100

    Draft for Object3D2

---
+++ src/scene/Object.cpp
@@ -0,0 +1,16 @@
+#include "Object.h"
+
+Object::Object()
+{
+	
+}
+
+Mesh3D* Object::GetMesh()
+{
+	return m_Mesh;
+}
+
+void Object::SetMesh(Mesh3D* m)
+{
+	m_Mesh = m;
+}
---
+++ src/scene/Object3D.cpp
@@ -0,0 +1,6 @@
+#include "Object3D.h"
+
+void Object3D::LoadDataFromFile(std::string path)
+{
+	std::cout << "Object3D::LoadDataFromFile is not implemented";
+}
